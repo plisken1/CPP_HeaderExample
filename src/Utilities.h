1d4bf552ca3b996719fd417925cb6ab89eaa15ff
Snake D Plissken <plisken1@users.noreply.github.com>
2020-09-13 07:16:09 +0100

    Update Utilities.h
    Updated to remove code and add link to current version of Utilities.h
    
    This is only a template of the header layout.
    
    Current version of Utilities.h can be found here: https://github.com/plisken1/my_cpp_includes/blob/master/my_includes/Utilities.h
    
    Utilities.h now forms part of the greater my_includes and can be found here: https://github.com/plisken1/my_cpp_includes

--- src/Utilities.h
+++ src/Utilities.h
@@ -1,69 +1,17 @@
 /*
  * Utilities Harry Nisbet
- * Initial Version 08/09/2020.
- * Version: 1.2.1
  *
+ * Get most up to date version from:
+ * https://github.com/plisken1/my_cpp_includes/blob/master/my_includes/Utilities.h
  */
 
 #ifndef UTILITIES_UTILITIES_H
 #define UTILITIES_UTILITIES_H
-#include <iostream>
-#include <fstream>
-
-extern std::string version;
 
 
-void writeLog(std::string fileName, std::string  message, bool append=true)
-{
-    std::fstream logFile;
-    if (append)
-    {
-        logFile.open(fileName.c_str(),std::fstream::out | std::fstream::app );
-        logFile << message << std::endl;
-    }
-    else
-    {
-        logFile.open(fileName.c_str(),std::fstream::out);
-        logFile << message << std::endl;;
-    }
-    logFile.close();
-}
-
-void print(std::string text, bool newLine=true)
-{
-    if (newLine)
-    {
-        std::cout << text<< std::endl;
-    }
-    else
-    {
-        std::cout << text;
-    }
-}
-
-std::string  getString(int number)
-{
-    return std::to_string(number);
-}
+// GET CURRENT VERSION OF Utilities.h
+// FROM: https://github.com/plisken1/my_cpp_includes/blob/master/my_includes/Utilities.h
 
 
-void sendEmail(std::string address, std::string message)
-{
-
-    char cmd[2000];
-    sprintf(cmd, "sendEmail -t %s -u \"Missing Stamping Details\" -m \"%s\" -s 172.16.1.140 -f harry.nisbet@libertysteelgroup.com", address.c_str(), message.c_str());
-    system(cmd);
-}
-
-void sendEmail(std::string to, std::string subject,
-               std::string message,std::string from, std::string smtp_IP)
-{
-
-    char cmd[2000];
-    sprintf(cmd, "sendEmail -t %s -u \"%s\" -m \"%s\" -s %s -f %s",
-                    to.c_str(), subject.c_str(),message.c_str(),
-                    smtp_IP.c_str(),from.c_str());
-    system(cmd);
-}
 
 #endif //UTILITIES_UTILITIES_H
